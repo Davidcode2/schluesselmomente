4a296bcf37ba4347969aed2843a174822a6273b3
Jakob Lingel <jlingel68@gmail.com>
2023-09-08 14:36:44 +0200

    Animation menu

--- src/index.js
+++ src/index.js
@@ -10,3 +10,38 @@ window.addEventListener("scroll", (event) => {
     backToTopButton.classList.add("hidden");
   }
 });
+
+var icon = document.getElementById("icon");
+var icon1 = document.getElementById("a");
+var icon2 = document.getElementById("b");
+var icon3 = document.getElementById("c");
+var nav = document.getElementById('nav');
+
+let navFlex = document.getElementById('nav-flex');
+
+icon.addEventListener('click', function() {
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+  nav.classList.toggle('hidden');
+});
+
+const para = document.querySelector("p");
+const mql = window.matchMedia("(max-width: 600px)");
+
+function screenTest(e) {
+  if (e.matches) {
+    icon.classList.remove('hidden');
+    nav.classList.add('hidden');
+    navFlex.classList.add('flex-col');
+  } else {
+    icon.classList.add('hidden');
+    nav.classList.remove('hidden');
+    navFlex.classList.remove('flex-col');
+    icon1.classList.remove('a');
+    icon2.classList.remove('c');
+    icon3.classList.remove('b');
+  }
+}
+
+mql.addEventListener("change", screenTest);
