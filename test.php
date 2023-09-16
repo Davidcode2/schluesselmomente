32b3e7472cc5eab07c9499e6a8842a344249b066
Jakob Lingel <jlingel68@gmail.com>
2023-09-16 14:57:34 +0200

    Test php files

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="./dist/output.css" rel="stylesheet">
+    <link href="./src/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    Hello there,
+
+    this is a test message inside a php file.
+
+    Best regards
+  </body>
+</html>
