530085fe03db493f2fe3fabe102e1f83f6aa57cb
Jakob Lingel <jlingel68@gmail.com>
2023-09-20 18:56:07 +0200

    js for animated menu

--- src/index.js
+++ src/index.js
@@ -28,20 +28,30 @@ function mobileMenu() {
     icon1.classList.toggle('a');
     icon2.classList.toggle('c');
     icon3.classList.toggle('b');
-    nav.classList.toggle('hidden');
+    nav.classList.toggle('invisible');
+    nav.classList.toggle('h-0');
+    nav.classList.toggle('h-screen');
+    nav.classList.toggle('opacity-0');
+    nav.classList.toggle('opacity-100');
   });
 
-  const para = document.querySelector('p');
   const mql = window.matchMedia('(max-width: 720px)');
 
   function screenTest(e) {
     if (e.matches) {
       icon.classList.remove('hidden');
-      nav.classList.add('hidden');
+      nav.classList.add('invisible');
+      nav.classList.add('opacity-100');
+      nav.classList.add('opacity-0');
+      nav.classList.add('h-0');
       navFlex.classList.add('flex-col');
     } else {
       icon.classList.add('hidden');
-      nav.classList.remove('hidden');
+      nav.classList.remove('invisible');
+      nav.classList.remove('h-0');
+      nav.classList.remove('h-screen');
+      nav.classList.remove('opacity-0');
+      nav.classList.remove('opacity-100');
       navFlex.classList.remove('flex-col');
       icon1.classList.remove('a');
       icon2.classList.remove('c');
@@ -52,6 +62,28 @@ function mobileMenu() {
   mql.addEventListener('change', screenTest);
 
   screenTest(mql);
+}
+
+function closeMobileMenu() {
+  const mql = window.matchMedia('(max-width: 720px)');
+  if (mql.matches) {
+    setTimeout(function() {
+      let nav = document.getElementById('nav');
+      let icon1 = document.getElementById('a');
+      let icon2 = document.getElementById('b');
+      let icon3 = document.getElementById('c');
+
+      nav.classList.add('invisible');
+      nav.classList.add('h-0');
+      nav.classList.remove('h-screen');
+      nav.classList.add('opacity-0');
+      nav.classList.remove('opacity-100');
+
+      icon1.classList.remove('a');
+      icon2.classList.remove('c');
+      icon3.classList.remove('b');
+    }, 1000);
+  }
 }
 
 function referenceView() {
