cc76164018ebc5b9eee72edecd10cca320964a39
Jakob Lingel <jlingel68@gmail.com>
2024-08-27 17:05:00 +0200

    Add tailwind config and errors.log

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}', 'index.html', 'impressum.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('./assets/wallhaven-4586m5.jpg')",
+        'footer-texture': "url('./assets/wallhaven-4586m5.jpg')",
+      },
+      boxShadow: {
+        'hard': '30px 30px rgba(0, 0, 0, 1)',
+      }
+    },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};
